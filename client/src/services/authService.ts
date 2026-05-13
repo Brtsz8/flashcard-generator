2ab5d87cfa5bd54d952c826344cd527e8000ed79
Brtsz8 <bartoszpacyga06@gmail.com>
2026-05-13 13:41:24 +0200

    AuthContext

---
+++ client/src/services/authService.ts
@@ -0,0 +1,39 @@
+import api from "../api/axios"
+
+export const login = async (
+    email: string,
+    password: string
+) => {
+    const response = await api.post(
+        "/auth/login",
+        {
+            email,
+            password
+        }
+    )
+
+    return response.data
+}
+
+export const register = async (
+    username: string,
+    email: string,
+    password: string,
+) => {
+    const response = await api.post(
+        "/auth/register",
+        {
+            username,
+            email,
+            password
+        }
+    )
+
+    return response.data
+}
+
+export const getMe = async () => {
+    const response = await api.get("/auth/me")
+
+    return response.data
+}
